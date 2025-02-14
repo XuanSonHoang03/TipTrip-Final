35dec21dd3499900e75e164a612da5ea5b8fe51e
lovering <lovering260103@gmail.com>
2025-02-14 13:35:28 +0700

    Login, Register, ForgotPass, ChangePass

---
+++ TipTrip/TipTrip/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TipTrip.Application.Implements.Interfaces;
+using TipTrip.Application.Implements.Services;
+using TipTrip.Common.Models;
+using TipTrip.IdentityFramework.DBContext;
+using TipTrip.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Identity services
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+	options.User.RequireUniqueEmail = true; // Đảm bảo email là duy nhất
+})
+	.AddEntityFrameworkStores<ApplicationDBContext>()
+    .AddDefaultTokenProviders();
+
+// Add application-specific services
+builder.Services.AddScoped<IMeAuthenticationService, AuthenticationService>();
+
+// Service Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Exprixe time
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// IHttpContextAccessor
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddTransient<IEmailService, EmailService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.MapRazorPages();
+app.Run();
